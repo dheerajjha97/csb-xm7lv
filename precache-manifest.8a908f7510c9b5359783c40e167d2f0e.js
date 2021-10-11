1d4c524fd154e43c2a030ff6cc11b87e37ff8e12
Code Sandbox <hello@codesandbox.io>
2021-10-11 13:50:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8a908f7510c9b5359783c40e167d2f0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaba73c92edbb3eab4268e1811bc41df",
+    "url": "https://dheerajjha97.github.io/csb-xm7lv/index.html"
+  },
+  {
+    "revision": "2a4594614855e7c8713e",
+    "url": "https://dheerajjha97.github.io/csb-xm7lv/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "1d259d9704d407b60a60",
+    "url": "https://dheerajjha97.github.io/csb-xm7lv/static/js/2.d24fad8e.chunk.js"
+  },
+  {
+    "revision": "28b5e57e1e89ae408eee8d8f4d692c7c",
+    "url": "https://dheerajjha97.github.io/csb-xm7lv/static/js/2.d24fad8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4594614855e7c8713e",
+    "url": "https://dheerajjha97.github.io/csb-xm7lv/static/js/main.72d79fe8.chunk.js"
+  },
+  {
+    "revision": "ec01b64f17e609366590",
+    "url": "https://dheerajjha97.github.io/csb-xm7lv/static/js/runtime-main.2380af65.js"
+  }
+]);
